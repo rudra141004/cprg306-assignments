ac4be0e6e885563d9259ef5d621da1e8469161d7
rudraa1410 <152832483+rudraa1410@users.noreply.github.com>
2024-09-17 05:29:11 -0600

    Week-2
    This Week-2 task contains page with my name and link of my git-hub repository

---
+++ app/Week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Page(){
+    return(
+        <div>
+            <h1>Rudra Solanki</h1>
+            <li>
+                <Link href="https://github.com/rudra141004/cprg306-assignments">My Github Repo Link
+                </Link>
+            </li>
+        </div>
+    )
+}
