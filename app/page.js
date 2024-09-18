94f8acb5bbec2216b9fc91778a254098c5dc09b8
rudraa1410 <152832483+rudraa1410@users.noreply.github.com>
2024-09-18 12:27:17 -0600

    Week-3

--- app/page.js
+++ app/page.js
@@ -6,6 +6,9 @@ export default function Page(){
       <li>
         <Link href="\week-2">Week 2</Link>
       </li>
+      <li>
+        <Link href="\week-3">Week-3</Link>
+      </li>
     </main>
   );
 }
