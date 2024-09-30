34c652d551017c9d8b5a1d70d292f2ac761b715b
rudraa1410 <152832483+rudraa1410@users.noreply.github.com>
2024-09-29 20:29:17 -0600

    Week-3
    This the final part of week 3 i have use tailwind css for design

---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import ItemList from "./item-list";
+export default function Page(){
+    return(
+        <main>
+            <h1 className="text-4xl">Shopping List</h1>
+            <ItemList/>
+        </main>
+
+    );
+    
+}
