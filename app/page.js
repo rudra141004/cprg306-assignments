d6d683ee0c68edc5b39c06f5e427ab8cbc838203
rudraa1410 <152832483+rudraa1410@users.noreply.github.com>
2024-10-06 19:22:02 -0600

    Week-4

--- app/page.js
+++ app/page.js
@@ -9,6 +9,12 @@ export default function Page(){
       <li>
         <Link href="\week-3">Week-3</Link>
       </li>
+      <li>
+        <Link href="\week-4">Week-4</Link>
+      </li>
+      <li>
+        <Link href="\week-5">Week-5</Link>
+      </li>
     </main>
   );
 }
---
+++ app/week-4/new-item.js
@@ -0,0 +1,43 @@
+"use client"
+
+import { useState } from 'react';
+
+export default function NewItem(){
+    const [quantity,setQuantity]=useState(1);
+
+    const increment = () => {
+        if (quantity <20){
+            setQuantity(quantity +1);
+        }
+    }
+
+    const decrement = ()=>{
+        if (quantity >1){
+            setQuantity(quantity -1);
+        }
+    }
+
+    return (
+        <div className="flex justify-center items-center space-x-4 mt-10">
+        <span className="text-xl font-bold">{quantity}</span>
+        <button
+          onClick={decrement}
+          disabled={quantity === 1}
+          className="bg-gray-300 text-white rounded-full px-4 py-2 disabled:bg-gray-500 focus:outline-none"
+        >
+          <span className="text-lg">−</span>
+        </button>
+  
+     
+        <button
+          onClick={increment}
+          disabled={quantity === 20}
+          className="bg-blue-500 text-white rounded-full px-4 py-2 disabled:bg-blue-300 focus:outline-none"
+        >
+          <span className="text-lg">+</span>
+        </button>
+      </div>
+    );
+
+
+}
